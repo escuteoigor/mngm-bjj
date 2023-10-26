1979eefe8f74ed403e915b8f781da11f7b9a380d
Igor <escuteoigor@gmail.com>
2023-10-25 21:10:02 -0300

    feat: Adicionado tema claro e provider.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter } from "react-router-dom";
+
 import { AppRoutes } from "./routes";
+import { Light } from "@mui/icons-material";
+import { LightTheme } from "./shared/themes";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
